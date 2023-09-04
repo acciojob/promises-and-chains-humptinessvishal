39ff8102f994e4ee89e5f4777ed305d444624775
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-09-04 09:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const userForm = document.getElementById("userForm");
+const ageInput = document.getElementById("age");
+const nameInput = document.getElementById("name");
+const submitButton = document.getElementById("btn");
+userForm.addEventListener("submit", function (e) {
+	e.preventDefault();
+	const age = ageInput.value;
+    const name = nameInput.value;
+	if (age >= 18) {
+		setTimeout(function () {
+			alert(`Welcome, ${name}. You can vote.`);
+		}, 4000);
+	} else {
+		alert(`Oh sorry ${name}. You aren't old enough.`);
+	}
+});
